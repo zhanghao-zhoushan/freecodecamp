2717cef82e36ccd532e11df544898ed9e81e18fd
张浩 <735296233@qq.com>
2017-12-16 12:06:47 +0800

    smallestCommons

--- smallestCommons.js
+++ smallestCommons.js
@@ -2,22 +2,22 @@
 // 思路 全部相乘 除以 最小公倍数
 function smallestCommons(arr) {
   arr.sort(function (a, b) {
-    a - b
-  })
-  var localArr = []
-  for (let i = arr[0]; i <= arr[1]; i++) {
-    localArr.push(i)
+    return a - b;
+  });
+  var localArr = [];
+  for (var i = arr[0]; i <= arr[1]; i++) {
+    localArr.push(i);
   }
   return localArr.reduce(function (a, b) {
-    return a * b / gcd(a, b)
-  })
+    return a * b / gcd(a, b);
+  });
 }
 
 function gcd(a, b) {
   if (a % b === 0) {
-    return b
+    return b;
   } else {
-    return gcd(b, a % b)
+    return gcd(b, a % b);
   }
 }
 console.log(smallestCommons([1, 13]))
