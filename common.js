a387658bb25f968ae012379783213f4e039516c2
张浩 <735296233@qq.com>
2017-12-16 14:54:12 +0800

    Drop it

--- common.js
+++ common.js
@@ -10,3 +10,20 @@ function find(arr, func) {
 find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 
 
+
+// Drop it
+function drop(arr, func) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+    if (!func(arr[0])) {
+      arr.shift();
+    } else {
+      return arr;
+    }
+  }
+  return [];
+}
+drop([1, 2, 3], function(n) {return n < 3; });
+
+
+
