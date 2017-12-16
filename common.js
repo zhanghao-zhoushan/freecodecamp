6313274a0247cae313148ac7428ea85e15393142
张浩 <735296233@qq.com>
2017-12-16 15:07:04 +0800

    Steamroller

--- common.js
+++ common.js
@@ -10,8 +10,8 @@ function find(arr, func) {
 find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 
 
-
 // Drop it
+
 function drop(arr, func) {
   var len = arr.length;
   for (var i = 0; i < len - 1; i++) {
@@ -23,7 +23,25 @@ function drop(arr, func) {
   }
   return [];
 }
+
 drop([1, 2, 3], function(n) {return n < 3; });
+
+
+// Steamroller
+
+function steamroller(arr) {
+    var result = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        result = result.concat(steamroller(arr[i]));
+      } else {
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+
+steamroller([1, [2], [3, [[4]]]]);
 
 
 
