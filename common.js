93bfb491a71556e4bd6812232340dc050063734c
张浩 <735296233@qq.com>
2017-12-16 15:23:41 +0800

    Binary Agents

--- common.js
+++ common.js
@@ -1,35 +1,36 @@
-// Finders Keepers
 
-function find(arr, func) {
-  var filtered = arr.filter(function (num) {
-    return func(num);
-  });
-  return filtered[0];
-}
+  // Finders Keepers
 
-find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
-
-
-// Drop it
-
-function drop(arr, func) {
-  var len = arr.length;
-  for (var i = 0; i < len - 1; i++) {
-    if (!func(arr[0])) {
-      arr.shift();
-    } else {
-      return arr;
-    }
+  function find(arr, func) {
+    var filtered = arr.filter(function (num) {
+      return func(num);
+    });
+    return filtered[0];
   }
-  return [];
-}
 
-drop([1, 2, 3], function(n) {return n < 3; });
+  find([1, 2, 3, 4], function (num) { return num % 2 === 0; });
 
 
-// Steamroller
+  // Drop it
 
-function steamroller(arr) {
+  function drop(arr, func) {
+    var len = arr.length;
+    for (var i = 0; i < len - 1; i++) {
+      if (!func(arr[0])) {
+        arr.shift();
+      } else {
+        return arr;
+      }
+    }
+    return [];
+  }
+
+  drop([1, 2, 3], function (n) { return n < 3; });
+
+
+  // Steamroller
+
+  function steamroller(arr) {
     var result = [];
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
@@ -41,7 +42,15 @@ function steamroller(arr) {
     return result;
   }
 
-steamroller([1, [2], [3, [[4]]]]);
+  steamroller([1, [2], [3, [[4]]]]);
 
-
-
+  // Binary Agents
+  function binaryAgent(str) {
+    var arr = str.split(' ');
+    for (var i = 0; i < arr.length; i++) {
+      var element = arr[i];
+      arr[i] = String.fromCharCode(parseInt(Number(element), 2));
+    }
+    return arr.join('');
+  }
+  binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
