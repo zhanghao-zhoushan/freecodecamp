0137fe8b7edf8f0888dd4e7d68af9252c6226fec
张浩 <735296233@qq.com>
2017-12-26 23:05:24 +0800

    change-name

--- exactChange.js
+++ exactChange.js
@@ -3,3 +3,23 @@
 // cid 是一个二维数组，存着当前可用的找零.
 // 当收银机中的钱不够找零时返回字符串 "Insufficient Funds". 如果正好则返回字符串 "Closed".
 // 否则, 返回应找回的零钱列表,且由大到小存在二维数组中.
+
+function checkCashRegister(price, cash, cid) {
+  var diff = cash - price;
+  var sum = 0;
+
+  for (var i = 0; i < cid.length - 1; i++) {
+    if (cid[i][1] === diff) return 'Closed'
+    sum += cid[i][1];
+  }
+
+  if (sum < diff) return 'Insufficient Funds';
+
+  cid.sort(function (a, b) {
+    return a[1] < b[1];
+  })
+
+  return cid.filter(function (item) {
+    return item[1] <= diff;
+  });
+}
