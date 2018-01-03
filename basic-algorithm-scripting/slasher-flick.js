2e92ac75f3f06761b3ec6bc7111b7870ee1a03dc
张浩 <735296233@qq.com>
2018-01-03 18:26:22 +0800

    Slasher Flick

--- basic-algorithm-scripting/slasher-flick.js
+++ basic-algorithm-scripting/slasher-flick.js
@@ -5,7 +5,11 @@
 // 返回一个数组被截断n个元素后还剩余的元素，截断从索引0开始。
 
 function slasher1(arr, howMany) {
-
+  if (arr.length <= howMany) {
+    return [];
+  } else if (howMany < 1) {
+    return arr;
+  }
   return arr.slice(howMany);
 
 }
@@ -13,6 +17,12 @@ function slasher1(arr, howMany) {
 // use splice
 // splice() 方法会直接对数组进行修改。
 function slasher(arr, howMany) {
+
+  if (arr.length <= howMany) {
+    return [];
+  } else if (howMany < 1) {
+    return arr;
+  }
 
   return arr.splice(howMany);
 
