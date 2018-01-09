75694b4b9798935aa8ec7a7dc09372138d58dc56
张浩 <735296233@qq.com>
2018-01-09 23:53:47 +0800

    Diff Two Arrays

---
+++ intermediate-algorithm-scripting/diff-two-arrays.js
@@ -0,0 +1,25 @@
+// Diff Two Arrays
+
+// 比较两个数组，然后返回一个新数组，该数组的元素为两个给定数组中所有独有的数组元素。换言之，返回两个数组的差异。
+
+// 如果你被难住了，记得使用 Read-Search-Ask尝试与他人结伴编程、编写你自己的代码。
+
+// 这是一些对你有帮助的资源:
+
+// Comparison Operators
+// Array.slice()
+// Array.filter()
+// Array.indexOf()
+// Array.concat()
+
+function diff(arr1, arr2) {
+
+  return arr1.filter(function (item) {
+    return arr2.indexOf(item) < 0
+  }).concat(arr2.filter(function (item) {
+    return arr1.indexOf(item) < 0
+  }))
+}
+
+var result = diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(result)
